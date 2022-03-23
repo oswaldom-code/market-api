fe61880881c8763422fa5e80c199749e829e7450
Oswaldo Montaño <oswaldoeliezer876@gmail.com>
2022-03-23 09:41:10 +0100

    first commit

---
+++ src/test/java/com/marketapi/MarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
